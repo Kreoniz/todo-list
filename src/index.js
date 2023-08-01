2da13e8e6cd65e6048cf2d169eb2625c07a134a8
Kreoniz <kreoniz@mail.ru>
2023-08-01 16:07:32 +0600

    Add mockup factory functions for notes and projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./style.css";
+
+function noteFactory(title, description, priority, dueDate) {
+    return {
+        title,
+        description,
+        priority,
+        dueDate,
+    };
+}
+
+function projectFactory(name) {
+    const notes = [];
+    const getNotes = () => notes;
+    const addNote = note => {
+        notes.push(note);
+    }
+
+    return {
+        getNotes,
+        addNote,
+        name,
+    };
+}
